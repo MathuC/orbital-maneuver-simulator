cd69480738e7c6abfc58bf16374f2cf8162a6908
MathuC <mathu99430@hotmail.com>
2024-12-25 00:57:31 -0500

    maneuvers now work but with many bugs: start arg and end arg are not working for transfer orbits and canvas fitting is not working either

--- core/utils.py
+++ core/utils.py
@@ -7,6 +7,69 @@ EARTH_MASS = 5.972e24
 def normalize_angle(angle):
     return angle % (2 * math.pi)
 
+def ellipse_bounding_box(a, e, theta):
+
+    #semi minor axis
+    b = a * math.sqrt(1 - (e ** 2))
+
+    # finding min, max values of x, y of the ellipse using derivatives of parametric equations
+    # this is done to perfectly fit the elliptical orbit in the simulation
+
+    # parametric equations of ellipse
+    # t represents eccentric anomaly technically here since an ellipse function is just a compressed/stretched circle function
+    def x(t):
+        return a * np.cos(theta) * np.cos(t) - b * np.sin(theta) * np.sin(t)
+    def y(t):
+        return a * np.sin(theta) * np.cos(t) + b * np.cos(theta) * np.sin(t)
+    
+    # function that returns the roots (t) of the derivatives of the parametric equations
+    def dx_dt_0(k: int):
+        return np.arctan(-b * np.tan(theta)/a) + k * np.pi
+    def dy_dt_0(k: int):
+        return np.arctan((b * np.cos(theta))/(a * np.sin(theta))) + k * np.pi
+    
+    x_dx_0 = [x(dx_dt_0(0)), x(dx_dt_0(1))]
+    y_dy_0 = [y(dy_dt_0(0)), y(dy_dt_0(1))]
+    
+    return [abs(x_dx_0[0] - x_dx_0[1]), abs(y_dy_0[0] - y_dy_0[1])]
+
+def max_length_earth_pos(orbits):
+    x_left = []
+    x_right = []
+    y_bottom = []
+    y_top = []
+    for orbit in orbits: 
+        if (orbit["ecc"] == 0):
+            for my_list in (x_left, x_right, y_bottom, y_top):
+                my_list.append(orbit["axis"])
+            continue
+        bounding_box = ellipse_bounding_box(orbit["axis"], orbit["ecc"], orbit["arg"])
+        semi_minor_axis =  orbit["axis"] * math.sqrt(1 - (orbit["ecc"] ** 2))
+        focal_distance = math.sqrt(orbit["axis"] ** 2 - semi_minor_axis ** 2) 
+
+        # we can use the focal distance because the center of the ellipse is centered in the bounding box
+        focal_distance_x = math.cos(orbit["arg"]) * focal_distance
+        focal_distance_y = math.sin(orbit["arg"]) * focal_distance
+        x_left.append(bounding_box[0]/2 + focal_distance_x)
+        x_right.append(bounding_box[0]/2 - focal_distance_x)
+        y_bottom.append(bounding_box[1] + focal_distance_y)
+        y_top.append(bounding_box[1] - focal_distance_y)
+
+    max_length_x = max(x_left) + max(x_right)
+    max_length_y = max(y_bottom) + max(y_top)
+
+    if (max_length_x >= max_length_y):
+        max_length = max_length_x
+        earth_x = max_length_x/2 - max(x_right)
+        earth_y = max_length_x/2 - (max_length_x-max_length_y)/2 - max(y_top)
+
+    else:
+        max_length = max_length_y
+        earth_x = max_length_y/2 - (max_length_y-max_length_x)/2 - max(x_right)
+        earth_y = max_length_y/2 - max(y_top)
+
+    return {"max_length" : max_length, "earth_pos": [earth_x, earth_y]}
+
 def process_orbit_data(semi_major_axis: int, ecc: float, arg: int) -> dict:
     semi_major_axis = int(semi_major_axis)
     ecc = float(ecc)
@@ -15,8 +78,7 @@ def process_orbit_data(semi_major_axis: int, ecc: float, arg: int) -> dict:
     if (ecc == 0 or arg % (math.pi/2) == 0): # circle or horizontal/vertical ellipse
         max_length = 2 * semi_major_axis
     else: 
-        semi_minor_axis = semi_major_axis * ((1 - (ecc ** 2)) ** 0.5)
-        max_length = max(ellipse_bounding_box(semi_major_axis, semi_minor_axis, arg))
+        max_length = max(ellipse_bounding_box(semi_major_axis, ecc, arg))
 
     return {"max_length": max_length}
 
@@ -31,7 +93,7 @@ def process_maneuver_data(start_orbit: dict, end_orbit: dict) -> dict:
     end_orbit["arg"] = math.radians(int(end_orbit["arg"]))
     
     # orbit stack
-    orbits = [{start_orbit.copy()}]
+    orbits = [start_orbit.copy()]
 
     # burn stack
     burns = []
@@ -86,9 +148,10 @@ def process_maneuver_data(start_orbit: dict, end_orbit: dict) -> dict:
         orbits.append(newOrbit)
     
     if (isEqual(orbits[-1], end_orbit)):
-        return {"orbits": orbits, "burns": burns, "max_length": None, "earth_pos": None}
+        max_length, earth_pos = max_length_earth_pos(orbits).values()
+        return {"orbits": orbits, "burns": burns, "max_length": max_length, "earth_pos": earth_pos}
     
-    # Make the orbit circular if needed to make arg the same later
+    # Make the orbit circular if need to change arg later
     if (orbits[-1]["arg"] != end_orbit["arg"] and normalize_angle(orbits[-1]["arg"] + math.pi) != end_orbit["arg"] 
         and orbits[-1]["ecc"] != 0):
 
@@ -110,7 +173,8 @@ def process_maneuver_data(start_orbit: dict, end_orbit: dict) -> dict:
         orbits.append(newOrbit)
 
     if (isEqual(orbits[-1], end_orbit)):
-        return {"orbits": orbits, "burns": burns, "max_length": None, "earth_pos": None}
+        max_length, earth_pos = max_length_earth_pos(orbits).values()
+        return {"orbits": orbits, "burns": burns, "max_length": max_length, "earth_pos": earth_pos}
 
     # Make the periapsis the same
     else:
@@ -130,26 +194,5 @@ def process_maneuver_data(start_orbit: dict, end_orbit: dict) -> dict:
         burns.append(v2-v1)
         orbits.append(newOrbit)
 
-    return {"orbits": orbits, "burns": burns, "max_length": None, "earth_pos": None}
-    
-def ellipse_bounding_box(a, b, theta):
-    # finding min, max values of x, y of the ellipse using derivatives of parametric equations
-    # this is done to perfectly fit the elliptical orbit in the simulation
-
-    # parametric equations of ellipse
-    # t represents eccentric anomaly technically here since an ellipse function is just a compressed/stretched circle function
-    def x(t):
-        return a * np.cos(theta) * np.cos(t) - b * np.sin(theta) * np.sin(t)
-    def y(t):
-        return a * np.sin(theta) * np.cos(t) + b * np.cos(theta) * np.sin(t)
-    
-    # function that returns the roots (t) of the derivatives of the parametric equations
-    def dx_dt_0(k: int):
-        return np.arctan(-b * np.tan(theta)/a) + k * np.pi
-    def dy_dt_0(k: int):
-        return np.arctan((b * np.cos(theta))/(a * np.sin(theta))) + k * np.pi
-    
-    x_dx_0 = [x(dx_dt_0(0)), x(dx_dt_0(1))]
-    y_dy_0 = [y(dy_dt_0(0)), y(dy_dt_0(1))]
-    
-    return [abs(x_dx_0[0] - x_dx_0[1]), abs(y_dy_0[0] - y_dy_0[1])]
+    max_length, earth_pos = max_length_earth_pos(orbits).values()
+    return {"orbits": orbits, "burns": burns, "max_length": max_length, "earth_pos": earth_pos}
